3c104139a5aa580b6da1e5d407f59f3346033790
Giovanni Bussi <giovanni.bussi@gmail.com>
2024-05-21 15:50:04 +0200

    added test that imports the module

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,7 +3,10 @@ import MDRefine
 
 class Test(unittest.TestCase):
     def test1(self):
+        # just test import
+        import MDRefine
         self.assertIsNone(None)
+        from MDRefine import Functions
     def test2(self):
         self.assertEqual(3,3)
     def test3(self):
