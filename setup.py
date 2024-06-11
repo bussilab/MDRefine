d510fb17655091587348438d1ce93cb581fad099
Ivan Gilardoni <igilardo@sissa.it>
2024-06-11 11:30:29 +0200

    improved setup so that it does not import MDRefine

--- setup.py
+++ setup.py
@@ -1,20 +1,35 @@
+import ast
 from setuptools import setup
+from pathlib import Path
 
 def readme():
-    with open('README.md') as f:
-        return f.read()
+    return Path('README.md').read_text()
+
+def extract_variable(file_path, variable_name):
+    with open(file_path, 'r') as f:
+        file_content = f.read()
+    module = ast.parse(file_content)
+    for node in ast.iter_child_nodes(module):
+        if isinstance(node, ast.Assign):
+            for target in node.targets:
+                if isinstance(target, ast.Name) and target.id == variable_name:
+                    return ast.literal_eval(node.value)
+    raise ValueError(f"Variable '{variable_name}' not found in {file_path}")
 
 def version():
-    from MDRefine import __version__ as version
-    return version
+    return extract_variable('MDRefine/_version.py', '__version__')
 
 def deps():
-    from MDRefine import _required_ as required
-    return required
+    return extract_variable('MDRefine/__init__.py', '_required_')
 
 def description():
-    from MDRefine import __doc__ as doc
-    return doc.partition('\n')[0]
+    with open('MDRefine/__init__.py', 'r') as f:
+        file_content = f.read()
+    module = ast.parse(file_content)
+    for node in ast.iter_child_nodes(module):
+        if isinstance(node, ast.Expr) and isinstance(node.value, ast.Str):
+            return node.value.s.split('\n')[0]
+    return ""
 
 setup(
     name="MDRefine",
