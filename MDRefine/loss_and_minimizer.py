14c63529458380fce76df67544131b0cdea30e5c
Ivan Gilardoni <igilardo@sissa.it>
2025-03-05 11:47:45 +0100

    added print references

--- MDRefine/loss_and_minimizer.py
+++ MDRefine/loss_and_minimizer.py
@@ -913,6 +913,7 @@ def print_references(alpha, beta, gamma, if_ddg):
     refs = ['Ensemble Refinement', 'Force-Field Fitting', 'Ensemble Refinement + Force-Field Fitting',
         'Ensemble + Forward-Model Refinement', 'Refinement with alchemical calculations', 'MDRefine package']
 
+    print(os.getcwd())
     if os.getcwd()[-8:] == 'Examples': path = '../MDRefine/references'
     else: path = 'references'
 
